5de7b15f8217ed14c5046365855b40aa3e9e3941
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-11-05 09:39:10 +0200

    Add extension method for CORS

---
+++ CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("MyPolicy", builder => builder
+                                                            .AllowAnyOrigin()
+                                                            .AllowAnyMethod()
+                                                            .AllowAnyHeader());
+            });
+        }
+    }
+}
