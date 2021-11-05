f22634a43b395fd36f1d463d7a665fee2b5bf678
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-11-05 16:54:34 +0200

    Repository pattern: create interface IRepositoryBase and abstract class RepositoryBase

---
+++ CompanyEmployees/Repository/RepositoryBase.cs
@@ -0,0 +1,44 @@
+﻿using Contracts;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected RepositoryContext _repositoryContext;
+
+        protected RepositoryBase(RepositoryContext repositoryContext)
+        {
+            _repositoryContext = repositoryContext;
+        }
+
+        public IQueryable<T> FindAll(bool trackChanges)
+            =>
+                !trackChanges
+                    ? _repositoryContext.Set<T>().AsNoTracking()
+                    : _repositoryContext.Set<T>();
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges)
+            =>
+                !trackChanges
+                    ? _repositoryContext.Set<T>()
+                        .Where(expression)
+                        .AsNoTracking()
+                    : _repositoryContext.Set<T>()
+                        .Where(expression);
+
+        public void Create(T entity)
+            =>
+                _repositoryContext.Set<T>().Add(entity);
+        public void Update(T entity)
+            =>
+                _repositoryContext.Set<T>().Update(entity);
+        public void Delete(T entity)
+            =>
+                _repositoryContext.Set<T>().Remove(entity);
+    }
+}
