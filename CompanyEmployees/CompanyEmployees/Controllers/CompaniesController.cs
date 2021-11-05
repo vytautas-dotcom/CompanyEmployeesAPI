d7f07aaf99c74b0849cfe0d3512150f001ed9769
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-11-05 18:44:59 +0200

    Try to get all companies from DB

---
+++ CompanyEmployees/CompanyEmployees/Controllers/CompaniesController.cs
@@ -0,0 +1,38 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly ILoggerManager _loggerManager;
+        private readonly IRepositoryManager _repositoryManager;
+
+        public CompaniesController(ILoggerManager loggerManager, IRepositoryManager repositoryManager)
+        {
+            _loggerManager = loggerManager;
+            _repositoryManager = repositoryManager;
+        }
+        [HttpGet]
+        public IActionResult GetCompanies()
+        {
+            try
+            {
+                var companies = _repositoryManager.Company.GetAllCompanies(false);
+                return Ok(companies);
+            }
+            catch (Exception ex)
+            {
+                _loggerManager.LogError($"Something went wrong in the ${nameof(GetCompanies)} action {ex}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
--- CompanyEmployees/Repository/CompanyRepository.cs
+++ CompanyEmployees/Repository/CompanyRepository.cs
@@ -16,5 +16,10 @@ namespace Repository
 
         }
 
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+            =>
+                FindAll(trackChanges)
+                .OrderBy(c => c.Name)
+                .ToList();
     }
 }
