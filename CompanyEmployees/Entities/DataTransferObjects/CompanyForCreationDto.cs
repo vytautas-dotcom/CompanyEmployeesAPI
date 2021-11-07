f1e13f80ca52a8e4a1ffc8a79f9fa2d30e668450
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-11-07 19:53:22 +0200

    Make POST company to be able post company together with the employees

--- CompanyEmployees/Entities/DataTransferObjects/CompanyForCreationDto.cs
+++ CompanyEmployees/Entities/DataTransferObjects/CompanyForCreationDto.cs
@@ -11,5 +11,7 @@ namespace Entities.DataTransferObjects
         public string Name { get; set; }
         public string Address { get; set; }
         public string Country { get; set; }
+
+        public IEnumerable<EmployeeForCreationDto> Employees { get; set; }
     }
 }
